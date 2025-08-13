4a8cc4766f861041f6bc9ed30e66564f8e3d6615
Nazi <banonazisharifi@gmail.com>
2025-08-13 22:07:35 +0430

    style Hether added

--- src/page/componnint/Heder.jsx
+++ src/page/componnint/Heder.jsx
@@ -1,7 +1,13 @@
 export default function Heder(){
     return(
         <div className="w-full h-fit flex justify-between items-center py-4 px-6">
-
+            <img src="../poblic/img/logo.webp" alt="" />
+            <ul className="flex space-x-3 font-bold">
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Contuct us</a></li>
+                <li><a href="#">Service</a></li>
+            </ul>
         </div>
     );
 }
