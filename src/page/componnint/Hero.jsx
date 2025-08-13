025fa17d8bdc1f8f7b50ee3c3c135dc0413495bf
Nazi <banonazisharifi@gmail.com>
2025-08-13 23:06:33 +0430

    style hero added

--- src/page/componnint/Hero.jsx
+++ src/page/componnint/Hero.jsx
@@ -1,8 +1,8 @@
 export default function Hero(){
     return(
         <div className="flex justify-between w-full h-fit p-6">
-            {/* <div className="w-1/2 shadow-sm p-4">
-            <img src="public/img/t-1.jpg" alt="" /></div> */}
+            <div className="w-1/2 shadow-sm p-4">
+            <img src="public/img/t-1.jpg" alt="" /></div>
         </div>
     )
 }
