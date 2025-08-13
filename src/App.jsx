263eb6ea51aa6eb2a3f31fab89e8514b76ea79f9
Nazi <banonazisharifi@gmail.com>
2025-08-13 22:10:56 +0430

    style Hether added

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,11 @@
+import Home from "./page/Home"
+
 function App() {
+    return(
+        <div>
+            <Home/>
+        </div>
+    )
 }
 
 export default App
--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,7 +1,9 @@
+import Heder from "./componnint/Heder";
+
 export default function Home(){
     return(
         <div>
-            
+            <Heder/>
         </div>
     );
 }
