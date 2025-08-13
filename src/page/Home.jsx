a413fe6264dcc8e3990207062eb627a64871157f
Nazi <banonazisharifi@gmail.com>
2025-08-13 23:09:52 +0430

    style hero added

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,9 +1,11 @@
 import Heder from "./componnint/Heder";
+import Hero from "./componnint/Hero";
 
 export default function Home(){
     return(
         <div>
             <Heder/>
+            <Hero/>
         </div>
     );
 }
--- src/page/componnint/Hero.jsx
+++ src/page/componnint/Hero.jsx
@@ -1,8 +1,8 @@
 export default function Hero(){
     return(
-        <div className="flex justify-between w-full h-fit p-6">
-            <div className="w-1/2 shadow-sm p-4">
-            <img src="public/img/t-1.jpg" alt="" /></div>
+        <div className="flex justify-between w-full h-[500px] p-6">
+            <div className="w-1/2 h-full shadow-sm p-4 rounded-sm flex items-center justify-center">
+            <img src="public/img/t-1.jpg" alt="" className="m-auto rounded-md w-[90%] h-[90%]" /></div>
         </div>
     )
 }
