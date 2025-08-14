0be95165bb2a73b665e388e40d8632ea8f18033f
Nazi <banonazisharifi@gmail.com>
2025-08-14 10:49:03 +0430

    style all Componnint  added

---
+++ src/page/componnint/Trending.jsx
@@ -0,0 +1,12 @@
+export default function Trending(){
+    return(
+        <div className="w-full h-screen flex justify-between">
+            <div className="w-[40%] h-full flex flex-col gap-4">
+                <h1 className="font-bold text-2xl">Trending Topic</h1>
+                <p>
+
+                </p>
+            </div>
+        </div>
+    )
+}
