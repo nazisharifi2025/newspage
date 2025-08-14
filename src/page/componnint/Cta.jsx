79af415fd4b9cf6630b3c54056d0017e3fe43afe
Nazi <banonazisharifi@gmail.com>
2025-08-14 11:34:07 +0430

    style all Componnint  added

---
+++ src/page/componnint/Cta.jsx
@@ -0,0 +1,9 @@
+export default function Cta(){
+    return(
+        <div className="w-full h-[40%] flex justify-center items-center gap-3.5">
+            <h1 className="font-bold text-4xl">Stay Updated with the Latest News</h1>
+            <h1 className="font-bold text-2xl">Don't Miss Out – Join Our Newsletter</h1>
+            <input type="email" placeholder="Enter yout email" className="border" />
+        </div>
+    )
+}
